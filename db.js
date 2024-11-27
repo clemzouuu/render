66affdd2a213a1164d9597eee69821a29c8846f7
clemzouuu <clemkinoa@gmal.com>
2024-11-27 15:08:59 +0100

    .

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  DATABASE_URL, 
+  `${process.env.DATABASE_URL}`, 
   {
     dialect: 'postgres',
     dialectOptions: {
