783a046666431b41c3b4488fe1648d3f49a38cab
clemzouuu <clemkinoa@gmal.com>
2024-11-27 14:50:43 +0100

    .

--- esgi.4a.render.nodejs/db.js
+++ esgi.4a.render.nodejs/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
+  'postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`', 
   {
     dialect: 'postgres',
     dialectOptions: {
